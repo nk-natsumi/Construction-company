dd4a0cde583076b0ef3f56982db77b14296b879f
nakayamanatsumi <ai.ntmtdr0606@gmail.com>
2025-06-08 21:45:37 +0900

    0608

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(".menu-trigger").click(function () {
         $(this).toggleClass("active");
-        $(".header__nav, .overlay").toggleClass("active");
+        $(".header__nav,.header__item , .header__sp-menu .header__link-title , .header__sub-menu , .header__item-more").toggleClass("active");
 
     })
 
@@ -12,9 +12,9 @@ $(document).ready(function () {
         spaceBetween: 48,
         allowTouchMove: false, // スワイプ無効
         autoplay: {
-          delay: 0, // 途切れなくループ
+            delay: 0, // 途切れなくループ
         },
-      });
+    });
 
 
 
@@ -33,37 +33,50 @@ $(document).ready(function () {
     switchViewport();
 
 
+    /* モーダル */
+    MicroModal.init({
+        openClass: 'is-open',
+        disableScroll: true,
+        onShow: (modal) => {
+            const container = modal.querySelector('.modal__container');
+            if (container) {
+                container.scrollTop = 0;
+            }
+        }
+    });
+
 });
 
 
-    // セッションストレージからフラグを取得
-    const isFirstLoad = sessionStorage.getItem('isFirstLoad');
 
-    // ページの読み込みが完了したときに実行される関数
-    window.addEventListener('load', function () {
-        // フラグが 'true' でない場合（初回アクセス時またはフラグが削除された場合）
-        if (isFirstLoad !== 'true') {
-            // ローディング画面を表示
-            const loadingElement = document.querySelector('.loading');
-            loadingElement.classList.add('active');
+// セッションストレージからフラグを取得
+const isFirstLoad = sessionStorage.getItem('isFirstLoad');
 
-            // 2秒後にローディング画面を非表示にする
-            setTimeout(function () {
-                // ローディング画面を非表示にする
-                loadingElement.classList.remove('active');
-                // コンテンツ要素を表示
-                const contentsElement = document.querySelector('.contents.hidden');
-                contentsElement.classList.remove('hidden'); // hiddenクラスを取り除くことでコンテンツを表示
-                // セッションストレージにフラグを保存
-                sessionStorage.setItem('isFirstLoad', 'true');
-            }, 2000);
-            setTimeout(function () {
-                loadingElement.style.display = 'none'; // 非表示にする
-            }, 2500);
-        } else {
-            // 2回目以降のアクセス時の処理を記述
+// ページの読み込みが完了したときに実行される関数
+window.addEventListener('load', function () {
+    // フラグが 'true' でない場合（初回アクセス時またはフラグが削除された場合）
+    if (isFirstLoad !== 'true') {
+        // ローディング画面を表示
+        const loadingElement = document.querySelector('.loading');
+        loadingElement.classList.add('active');
+
+        // 2秒後にローディング画面を非表示にする
+        setTimeout(function () {
+            // ローディング画面を非表示にする
+            loadingElement.classList.remove('active');
             // コンテンツ要素を表示
             const contentsElement = document.querySelector('.contents.hidden');
             contentsElement.classList.remove('hidden'); // hiddenクラスを取り除くことでコンテンツを表示
-        }
-    });
+            // セッションストレージにフラグを保存
+            sessionStorage.setItem('isFirstLoad', 'true');
+        }, 2000);
+        setTimeout(function () {
+            loadingElement.style.display = 'none'; // 非表示にする
+        }, 2500);
+    } else {
+        // 2回目以降のアクセス時の処理を記述
+        // コンテンツ要素を表示
+        const contentsElement = document.querySelector('.contents.hidden');
+        contentsElement.classList.remove('hidden'); // hiddenクラスを取り除くことでコンテンツを表示
+    }
+});
